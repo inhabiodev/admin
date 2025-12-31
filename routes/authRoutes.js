c113938528e3f917ccbf5489b355082c8dc2661e
inhabiodev <rauf.zeynalov@inhabio.com>
2025-12-31 11:57:30 +0400

    Update authRoutes.js
    Blocked for registering new user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
-router.post('/register', authController.register);
+//router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/verify', protect, authController.verifyToken);
 
